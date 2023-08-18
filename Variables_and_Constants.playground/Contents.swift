58ea96e95ff4ed628956ec6112eb61dd61c5885d
AndreRodriguezSabino <andresabinocalvo@gmail.com>
2023-08-18 21:06:28 +0100

    First update on the code

--- Variables_and_Constants.playground/Contents.swift
+++ Variables_and_Constants.playground/Contents.swift
@@ -21,4 +21,24 @@ print(totalDistance_Km)
 
 // CREATING STRINGS
 
+/*
+ -> Text data assigned to a variable or constant, we call it
+ STRING, String is grouped of character (text) between double quote
+ that we use as data.
+ 
+ -> Swift does not allow the creation of a String with multiple
+ lines using double quotes, instead, you need to use triple quotes
+ if you want to create a String with multiple line, however, when
+ using multi-lines String after the first triple quote you need
+ to 'break' the first line, the following text needs indentation
+ and the second and last three quotes 'breaks' after text
+ as you can see on the example below:
+ */
 
+var bigText = """
+ This is an example how you can create
+ a String using multiple lines inside
+ of a variable or constant, in this case,
+ in a variable.
+ """
+print(bigText)

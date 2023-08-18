eeebddf0956f0eccdd406d1d04d9816e23f4be00
AndreRodriguezSabino <andresabinocalvo@gmail.com>
2023-08-18 20:33:10 +0100

    Swift Variable, Constants, Types (Strings and Number)

---
+++ Variables_and_Constants.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+var greeting = "Hello, world!"
+/* VAR is a keyword for a variable creation.
+ Variable is a kind of 'container' where you can store data
+ for usage on different points of your software, also variables
+ can be changed any time with a different data in it, if you declare,
+ as for example 'greeting = "Andre". */
+
+let totalDistance_Km = 400
+/* LET, also is a keyword for a constant creation unlike variable,
+ this means once you set a data up, the data can not be changed
+ and if you try to do it you are going to receive an error message
+ and your software will stopped working. */
+
+print(greeting)
+print(totalDistance_Km)
+/* PRINT is a function that normally used to print data from
+ a variable, constant or any type of message*/
+
+
+// CREATING STRINGS
+
+

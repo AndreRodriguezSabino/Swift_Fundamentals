1e70d12016af5c083cfe04793ad7cebdcfbd3754
AndreRodriguezSabino <andresabinocalvo@gmail.com>
2023-08-19 10:16:36 +0100

    Update v3, Decimal numbers and data type introduction.

--- Variables_and_Constants.playground/Contents.swift
+++ Variables_and_Constants.playground/Contents.swift
@@ -74,3 +74,32 @@ let division = examRate / 2
 var points = 8
 points = points + 3
 points += 7 // Here is tha same example of above but using COMPOUND OPERATOR
+
+
+// USE OF DECIMAL NUMBERS
+
+/* When we are working with numbers a huge amount of the time we need
+ to treat this element as a decimal numbers (floating point after one number).
+ As mentioned previously Swift is a programming language with treat
+ variables/constant Types very strictly and them can not be mixed,
+ in the case of Decimal numbers the type that we have to use is DOUBLE.
+ See some examples:
+ */
+
+let num0 = 1.3
+let num1: Double = 5.9 // Double type declared in the example
+let result: Double = num0 + num1
+print(result)
+
+/* However, if you try to sum an Integer number with a decimal
+ number you are going to receive an error message saying you are not
+ allowed to mix the types up, in order to granted an operation
+ using this two numbers, you should use numbers of the same types
+ or you force a chenge of type, as you can see on the following
+ example:
+ */
+
+let num2 = 6.0
+let num3 = 3
+let IntForced = Int(num2) + num3 // Int(num2) is a function to force the type conversion
+print(IntForced)

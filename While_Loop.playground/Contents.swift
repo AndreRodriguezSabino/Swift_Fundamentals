178d5797552824bc09c14af7025c83368219cfdf
AndreRodriguezSabino <andresabinocalvo@gmail.com>
2023-09-03 13:10:39 +0100

    While Loop v2

--- While_Loop.playground/Contents.swift
+++ While_Loop.playground/Contents.swift
@@ -17,3 +17,38 @@ while countDown >= 0 {
     countDown -= 1
 }
 print("Blast off!!!")
+
+var countUp = 0
+while countUp <= 60 {
+    print(countUp)
+    countUp += 6
+}
+
+/*
+ While loops come in handy when you're uncertain about the number of times
+ the loop needs to run. To illustrate this, I'd like to introduce you to a
+ valuable feature shared by Int and Double in Swift: the random(in:) function.
+ By providing it with a range of numbers, this function will return a random
+ Int or Double value within that specified range.
+ */
+
+var id = Int.random(in: 1...500)
+var amount = Double.random(in: 0...2)
+
+// create an integer to store the table position
+var table = 0
+
+//carry on looping until we reach 6
+while table != 6 {
+    //roll a new dice and print what it was
+    table = Int.random(in: 1...6)
+    print("Rolling dice \(table)")
+}
+
+/*
+ In your coding projects, you will probably use both for and while loops.
+ For loops are typically preferred when you have a known quantity of data
+ to iterate through, such as a range or an array. Conversely, while loops
+ are well-suited when you need to apply a particular custom condition to
+ govern the loop's execution.
+ */

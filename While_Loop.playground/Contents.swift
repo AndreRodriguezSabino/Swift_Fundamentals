7e64be2b87da5423e1d6905e57d91daa2ac6576a
AndreRodriguezSabino <andresabinocalvo@gmail.com>
2023-09-03 14:33:20 +0100

    While Loop v4

--- While_Loop.playground/Contents.swift
+++ While_Loop.playground/Contents.swift
@@ -81,3 +81,50 @@ while speed <= 55 {
     print("Accelerating to \(speed)")
     speed += 1
 }
+
+/*
+ In Swift, you have two options for skipping one or more items in a loop:
+ "continue" allows you to skip the current loop iteration, while "break"
+ lets you skip all remaining iterations. Similar to while loops, these features
+ are occasionally employed but are less common in practice than you might expect.
+
+ Let's delve into them individually, starting with "continue." When you're
+ iterating over an array of data in Swift, the language extracts one item at
+ a time from the array and executes the loop body with that item. If you use
+ "continue" within the loop body, Swift will promptly halt the current loop
+ iteration and proceed to the next item in the loop, where it will continue
+ its normal execution. This is frequently employed near the beginning of loops
+ to filter out loop variables that do not meet specific criteria you've defined.
+ */
+
+let extensionNames = ["Andre.jpg", "examples.txt", "Lucas.jpg", "MyCV.psd"]
+
+for extensionName in extensionNames {
+    if extensionName.hasSuffix(".jpg") == false {
+        continue
+    }
+    print("The files found were \(extensionName)")
+}
+
+/*
+ That creates an array of extension strings, then loops over each one and checks
+ to make sure it has the suffix “.jpg” – that it’s a picture. continue is used
+ with all the extensions failing that test, so that the rest of the loop body is
+ skipped.
+ */
+
+// As for break, that exits a loop immediately and skips all remaining iterations.
+
+let num1 = 10
+let num2 = 16
+var multiple = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: num1) && i.isMultiple(of: num2) {
+        multiple.append(i)
+        if multiple.count == 10 {
+            break
+        }
+    }
+}
+print(multiple)

1a74566ca5ede0553d996d26669794e33d04f387
AndreRodriguezSabino <andresabinocalvo@gmail.com>
2023-10-11 18:38:49 +0100

    Struct some examples

--- Structs.playground/Contents.swift
+++ Structs.playground/Contents.swift
@@ -1,3 +1,22 @@
 import Cocoa
 
 // STRUCTS
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+    
+    func printSummary() {
+        print("\(title), \(year) by \(artist)")
+    }
+}
+
+let bestOfTheBeast = Album(title: "Best of the Beast", artist: "Iron Maiden", year: 1996)
+let aceOfSpades = Album(title: "Ace of Spades", artist: "Motorhead", year: 1980)
+
+print(bestOfTheBeast.artist)
+print(aceOfSpades.artist)
+
+bestOfTheBeast.printSummary()
+aceOfSpades.printSummary()

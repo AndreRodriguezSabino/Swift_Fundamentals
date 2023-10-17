fd9f9cd1a6e4ee9c8cd549791d8f3f21aeabfa90
AndreRodriguezSabino <andresabinocalvo@gmail.com>
2023-10-17 12:07:01 +0100

    Static Struct Explaining

--- Struct_Part_Two.playground/Contents.swift
+++ Struct_Part_Two.playground/Contents.swift
@@ -83,3 +83,62 @@ if success {
  Note: When using private access control for one or more properties, you will likely need 
  to create your own initializer.
  */
+
+//Static properties and methods
+/*
+ You've learned how to attach properties and methods to structs, and each struct maintains its 
+ distinct copy of these properties. This ensures that calling a method on one struct won't access
+ the properties of a different struct of the same type.
+
+ However, there are situations, albeit infrequent, where you'd like to add a property or method 
+ to the struct itself, rather than to a specific instance of the struct. This enables you to use
+ these properties and methods directly. I frequently employ this technique in SwiftUI for two main
+ purposes: generating example data and storing fixed data that should be accessible from various
+ locations.
+
+ To begin, let's explore a simplified example of how to create and utilize static properties and 
+ methods:
+ */
+
+struct School {
+    static var studentCount = 0
+    
+    static func add(student: String) {
+        print("Student \(student) added to the new course.")
+        studentCount += 1
+    }
+}
+
+/*
+ Pay attention to the "static" keyword in there. It signifies that both the "studentCount" property 
+ and the "add()" method are associated with the School struct as a whole, rather than with specific
+ instances of the struct.
+
+ To employ this code, you would do the following:
+ */
+School.add(student: "Andre Rodriguez")
+print(School.studentCount)
+
+/*
+ I haven't created an instance of School - we can directly use "add()" and "studentCount" on the struct 
+ itself. This is because both of them are static, which means they don't exist uniquely on instances of
+ the struct.
+
+ This also explains why we can modify the "studentCount" property without needing the "mutating" keyword. 
+ The "mutating" keyword is typically required for regular struct functions, but in this case, there's no
+ need for it because we don't create an instance when calling "add()".
+
+ If you want to combine static and non-static properties and methods, keep in mind two rules:
+
+ 1 - To access non-static code from static code, it's not possible. Static properties and methods cannot 
+ refer to non-static properties and methods because it doesn't make sense - which instance of School would
+ you be referring to?
+ 2 - To access static code from non-static code, always use your type's name, such as "School.studentCount." 
+ You can also use "Self" to refer to the current type.
+ Now, we have "self" and "Self," and they have distinct meanings: "self" pertains to the current value of
+ the struct, while "Self" relates to the current type.
+
+ A helpful tip: It's easy to overlook the distinction between "self" and "Self," but if you think about it, 
+ it aligns with the naming conventions in Swift, where data types begin with a capital letter
+ (e.g., Int, Double, Bool). So, it's logical for "Self" to start with a capital letter too.
+ */
